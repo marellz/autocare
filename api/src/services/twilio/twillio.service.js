e6cc6b6114b20dc7002f47ee79b549a0b384d2e5
Dave Njoroge <davenjoroge10@gmail.com>
2025-05-20 23:33:59 +0300

    twilio service

---
+++ api/src/services/twilio/twillio.service.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import twilio from "twilio";
+
+dotenv.config();
+
+const accountSid = process.env["TWILIO_SID"];
+const authToken = process.env["TWILIO_AUTH_TOKEN"];
+const twillioWhatsappNumber = process.env["TWILIO_WHATSAPP_NUMBER"];
+
+export const client = twilio(accountSid, authToken);
+
+export const sendWhatsapp = async ({ to, body }) => {
+  const from = `whatsapp:${twillioWhatsappNumber}`;
+  client.messages
+    .create({
+      from,
+      to,
+      body,
+    })
+    .then((message) => console.log(`Message sent!`, message.sid))
+    .catch((err) => console.error(`Error sending: ${err}`));
+};
