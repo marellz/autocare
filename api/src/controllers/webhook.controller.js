6c47049ca6b1fe02a5cd14b4371c2dc2bb86f6f9
Dave Njoroge <davenjoroge10@gmail.com>
2025-05-21 20:22:19 +0300

    updated webhook controller

--- api/src/controllers/webhook.controller.js
+++ api/src/controllers/webhook.controller.js
@@ -1,56 +1,32 @@
-import { sendWhatsapp } from "#services/twilio/twillio.service.js";
+import ReceiverService from "#services/receiver/receiver.service.js";
 
 export default class WebHookController {
   static async receive(req, res) {
-    const { From, Body, ProfileName } = req.body;
+    const { From: sender, Body, ProfileName: name, WaId: phone } = req.body;
 
-    const sender = From;
-    const name = ProfileName;
+    
 
-    // find pending conversation with sender.
-    // if there was a pending conversation: repeat:
-    if(pendingRequest){
-        // updateRequest
+    /**
+     * FROM A VENDOR TO AN EXISTING REQUEST
+     */
 
-        // grab pending details, fill in. if missing, repeat. if not, close conversation.
+    // find if there was an existing "pending" request and the phone matches one of the requested vendors
 
-        return
-    }
-    // else: new
+    const existingRequest = false; //await ReceiverService.findExistingRequest(phone);
 
-    // pass to AI to discern what they are looking for from the body
-
-    const { item, pending } = ParserService.parseRequest(Body);
-
-    if (pending) {
-      const pendingList = pending.join(",");
-
-      // update conversation
-      await sendWhatsapp({
-        to: From,
-        body: "Still missing a few details: " + pendingList,
-      });
-
-      res.status(200);
+    if (existingRequest) {
+      /**
+       * HANDLE INPUT AS A VENDOR RESPONSE
+       */
+      ReceiverService.handleVendorResponse(existingRequest);
     } else {
-      await sendWhatsapp({
-        to: From,
-        body: "Thank you for your request. Well take it from here and get back soon.",
-      });
-      
-      // complete conversation
+      /**
+       * HANDLE REQUEST AS A CLIENT REQUEST
+       */
 
-      res.status(200);
+      ReceiverService.handleClientRequest({ sender, name, phone, Body });
     }
 
-    // pass it to vendorRequestService.create
-
-    await sendWhatsapp({
-      from: "whatsapp:+14155238886",
-      to: req.body.From,
-      body: `Hello ${req.body.ProfileName}`,
-    });
-
-    res.send("<Response></Response>"); // WhatsApp expects a 200 XML response
+    return;
   }
 }
