5da8beea7df8f4a6152827d3a2ee971ef5eebbf0
Dave Njoroge <davenjoroge10@gmail.com>
2025-05-22 22:09:59 +0300

    update request model

--- api/src/db/models/request.model.ts
+++ api/src/db/models/request.model.ts
@@ -1,4 +1,4 @@
-import { DataTypes } from 'sequelize';
+import { DataTypes } from "sequelize";
 export default {
   id: {
     type: DataTypes.INTEGER,
@@ -37,7 +37,7 @@ export default {
     allowNull: false,
     type: DataTypes.ARRAY(DataTypes.STRING),
   },
-  
+
   // missing_details, pending, completed
   status: {
     allowNull: false,
@@ -50,24 +50,38 @@ export default {
 };
 
 export enum CarPartDetailEnum {
-  PART_NAME = 'partName',
-  CAR_BRAND = 'carBrand',
-  CAR_MODEL = 'carModel',
-  CAR_VARIANT = 'carVariant',
-  CAR_YEAR = 'carYear',
+  PART_NAME = "partName",
+  CAR_BRAND = "carBrand",
+  CAR_MODEL = "carModel",
+  CAR_VARIANT = "carVariant",
+  CAR_YEAR = "carYear",
+  ENGINE_SIZE = "engineSize",
+  TRANSMISSION = "transmission",
+  BODY_TYPE = "bodyType",
 }
 
 export type CarPartDetail = `${CarPartDetailEnum}`;
 
 export const carPartDetailLables: Record<keyof CapturedDetails, string> = {
-  [CarPartDetailEnum.PART_NAME]: 'part name',
-  [CarPartDetailEnum.CAR_BRAND]: ' car brand/make',
-  [CarPartDetailEnum.CAR_MODEL]: 'car model',
-  [CarPartDetailEnum.CAR_VARIANT]: 'car variant',
-  [CarPartDetailEnum.CAR_YEAR]: 'car year or generation',
+  [CarPartDetailEnum.PART_NAME]: "Part name",
+  [CarPartDetailEnum.CAR_BRAND]: " Car brand/make e.g BMW, Mercededs, Toyota",
+  [CarPartDetailEnum.CAR_MODEL]:
+    "Car model e.g 3-series, C-class, Mark X, Corolla",
+  [CarPartDetailEnum.CAR_VARIANT]: "Car variant e.g 320i, C200, 250G, 5A/7A",
+  [CarPartDetailEnum.CAR_YEAR]: "Car year e.g 2001",
+  [CarPartDetailEnum.TRANSMISSION]: "Transmission type e.g Automatic, Manual",
+  [CarPartDetailEnum.ENGINE_SIZE]: "Car engine size in cc e.g 2000",
+  [CarPartDetailEnum.BODY_TYPE]: "Car body type e.g Sedan, Coupe, Convertible",
 };
 
 export type CapturedDetails = Record<CarPartDetail, string | null>;
+
+export enum RequestStatusEnum {
+  MISSING_DETAILS = "missing_details",
+  PENDING = "pending",
+  COMPLETED = "completed",
+}
+export type RequestStatus = `${RequestStatusEnum}`;
 
 export interface Request {
   id: string;
@@ -78,7 +92,7 @@ export interface Request {
   originalMessages: string[];
   capturedDetails: Partial<CapturedDetails>;
   missingDetails: string[];
-  status: string;
+  status: RequestStatus;
   fulfilled_at: string | null;
 }
 
@@ -90,6 +104,6 @@ export interface NewRequest {
   originalMessages: string[];
   capturedDetails: Partial<CapturedDetails>;
   missingDetails: string[];
-  status: string;
+  status: RequestStatus;
   fulfilled_at?: string | null;
 }
