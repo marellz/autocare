4f7fa3064cc44750dad9991f067d18824e7a25f0
Dave Njoroge <davenjoroge10@gmail.com>
2025-06-04 00:21:57 +0300

    update vendorRequest logic

--- api/src/router/routes/_webhookFaker.ts
+++ api/src/router/routes/_webhookFaker.ts
@@ -11,15 +11,44 @@ router.post("/", async (req: Request, res: Response) => {
 
   // find if there was an existing "pending" request and the phone matches one of the requested vendors
 
-  const existingRequest = false; //await ReceiverService.findExistingRequest(phone);
+  const existingVendorRequests = (
+    await ReceiverService.findExistingRequests(phone)
+  ).map((vr) => vr.get());
 
-  if (existingRequest) {
+  console.log({ existing: existingVendorRequests.length });
+  
+  if (existingVendorRequests.length) {
     /**
      * HANDLE INPUT AS A VENDOR RESPONSE
      */
-    ReceiverService.handleVendorResponse(existingRequest, Body);
 
-    res.json({ to: "vendor", response: "Nothing yet" });
+    let response =
+      "We could not find a matching request for your response. Please try again.";
+
+    if (existingVendorRequests.length === 1) {
+      response = await ReceiverService.handleVendorResponse(
+        existingVendorRequests[0],
+        Body,
+      );
+    } else {
+      const disambiguatedResponse =
+        await ReceiverService.disambiguateVendorResponse(
+          existingVendorRequests,
+          Body,
+        );
+
+      if (disambiguatedResponse) {
+        response = await ReceiverService.handleVendorResponse(
+          disambiguatedResponse,
+          Body,
+        );
+      } else {
+        // todo: add as unresolved.. or treat as new request.
+        response = "Could not disambiguate your response. Please try again."; // todo: use a proper reference.
+      }
+    }
+
+    res.json({ to: "vendor", response });
   } else {
     /**
      * HANDLE REQUEST AS A CLIENT REQUEST
