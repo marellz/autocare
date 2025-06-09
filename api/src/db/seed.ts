5675583bb827098009501e6c841dfc47f184b5dc
Dave Njoroge <davenjoroge10@gmail.com>
2025-06-09 18:55:05 +0300

    update seeder, only seed vendors

--- api/src/db/seed.ts
+++ api/src/db/seed.ts
@@ -35,7 +35,7 @@ dotenv.config();
 
   // 1. Create Vendors
   try {
-    const vendors = await Vendor.bulkCreate(
+    await Vendor.bulkCreate(
       Array.from({ length: 5 }, () => ({
         name: faker.company.name(),
         phone: faker.phone.number(),
@@ -43,41 +43,6 @@ dotenv.config();
       })),
     );
 
-    // 2. Create Requests
-    const requests = await Request.bulkCreate(
-      Array.from({ length: 3 }, () => ({
-        name: faker.person.fullName(),
-        phone: faker.phone.number(),
-        channel: "whatsapp",
-        originalMessages: [faker.lorem.sentence(), faker.lorem.sentence()],
-        item: faker.commerce.productName(),
-        capturedDetails: {},
-        missingDetails: [],
-        status: faker.helpers.arrayElement([
-          RequestStatusEnum.PENDING,
-          RequestStatusEnum.MISSING_DETAILS,
-        ]),
-        fulfilled_at: null,
-      })),
-    );
-
-    // 3. Link vendors to requests (many-to-many with VendorRequest)
-    for (const request of requests) {
-      // randomly assign 2 vendors to each request
-      const sampledVendors = faker.helpers.arrayElements(vendors, 2);
-
-      for (const vendor of sampledVendors) {
-        const _vendor = vendor.get();
-        const _request = request.get();
-
-        await VendorRequest.create({
-          vendorId: _vendor.id,
-          requestId: _request.id,
-          price: null,
-          condition: null,
-        });
-      }
-    }
     console.log("✅ Seed complete");
     process.exit(0);
   } catch (error) {
