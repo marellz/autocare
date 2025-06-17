5476861d52f7c2dd959c2350dbe9ba9ed63d563d
Dave Njoroge <davenjoroge10@gmail.com>
2025-06-17 22:28:06 +0300

    update default layout

--- web/src/layouts/Default.tsx
+++ web/src/layouts/Default.tsx
@@ -1,17 +1,12 @@
 import type { ReactNode } from 'react'
 import { NavLink } from 'react-router-dom'
 import RequestForm from '../components/partials/RequestForm'
-
 interface Props {
   children: ReactNode
 }
 
 const DefaultLayout = ({ children }: Props) => {
   const headerLinks = [
-    {
-      path: '/',
-      label: 'Home',
-    },
     {
       path: '/about',
       label: 'About',
@@ -30,58 +25,33 @@ const DefaultLayout = ({ children }: Props) => {
   return (
     <>
       <header>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container">
-            <NavLink className="navbar-brand" to="/">
-              Autocare
-            </NavLink>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0 align-items-lg-center">
-                {headerLinks.map((link, i) => (
-                  <li className="nav-item" key={i}>
-                    <NavLink
-                      className={({ isActive }) =>
-                        ['nav-link', isActive && 'text-primary'].join(' ')
-                      }
-                      to={link.path}
-                    >
-                      {link.label}
-                    </NavLink>
-                  </li>
-                ))}
+        <div className="container mx-auto py-4">
+          <ul className="flex items-center space-x-2">
+            <li>
+              <NavLink to="/" className="text-lg font-bold">Autocare</NavLink>
+            </li>
+            {headerLinks.map((link, i) => (
+              <li className="" key={i}>
+                <NavLink
+                  className={({ isActive }) => ['py-2 px-2', isActive && 'bg-gray-100'].join(' ')}
+                  to={link.path}
+                >
+                  {link.label}
+                </NavLink>
+              </li>
+            ))}
 
-                <li className="nav-item nav-link">
-                  <button
-                    className="btn btn-outline-success"
-                    type="button"
-                    data-bs-toggle="modal"
-                    data-bs-target="#requestFormModal"
-                  >
-                    Make request
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        <RequestForm></RequestForm>
+            <li className="!ml-auto">
+              <RequestForm></RequestForm>
+            </li>
+          </ul>
+        </div>
       </header>
-      <main className='flex-grow-1'>
-        <div className="container">{children}</div>
+      <main className="flex-auto">
+        <div className="container mx-auto">{children}</div>
       </main>
       <footer>
-        <div className="container">
+        <div className="container mx-auto">
           <p>Footer</p>
         </div>
       </footer>
