542005a45f31d0c5dd2329dd3933c1bbca3fd810
Dave Njoroge <davenjoroge10@gmail.com>
2025-06-17 21:12:41 +0300

    installed tailwind/shadcn

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        silenceDeprecations: [
-          "import",
-          "mixed-decls",
-          "color-functions",
-          "global-builtin",
-        ],
+        silenceDeprecations: ['import', 'mixed-decls', 'color-functions', 'global-builtin'],
       },
     },
   },
-});
+})
