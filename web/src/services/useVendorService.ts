99341e59753171ba217d5e3b31c38eb9690e9231
Dave Njoroge <davenjoroge10@gmail.com>
2025-06-18 23:15:27 +0300

    vendor service update, new methods

--- web/src/services/useVendorService.ts
+++ web/src/services/useVendorService.ts
@@ -10,11 +10,10 @@ export interface Vendor {
 }
 
 export interface NewVendor {
-  id: number
   name: string
   phone: string
   brands: string[]
-  location?: string
+  location?: string | null
 }
 
 export const useVendorService = () => {
@@ -52,19 +51,16 @@ export const useVendorService = () => {
   const createVendor = async (vendorData: NewVendor) => {
     setLoading(true)
     try {
-      const response = await api.get('/vendors', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(vendorData),
+      const response = await api.post('', {
+        json: vendorData,
       })
+
       if (!response.ok) {
         throw new Error('Network response was not ok')
       }
-      
+
       const { data } = await response.json<{ message: 'ok'; data: Vendor }>()
-      if(data) setVendors([...vendors, data])
+      if (data) setVendors((prev) => [...prev, data])
       return data
     } catch (error) {
       console.error('Failed to create vendor:', error)
@@ -75,9 +71,50 @@ export const useVendorService = () => {
     }
   }
 
-  useEffect(() => {
-      getVendors()
-    }, [])
+  const updateVendor = async (id: number, vendorData: NewVendor) => {
+    setLoading(true)
+    try {
+      const response = await api.put(id.toString(), {
+        json: vendorData,
+      })
+      if (!response.ok) throw new Error('Error updating vendor')
 
-  return { vendors, loading, error, getVendors, createVendor }
+      const { error } = await response.json<{ error?: string; message: 'ok' | 'error' }>()
+      if (error) throw new Error(error)
+
+      // update vendors
+      setVendors((prev) => prev.map((v) => (v.id === id ? { ...v, ...vendorData } : v)))
+    } catch (error) {
+      console.error('Error deleting vendor', error)
+      setError(error as string)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const deleteVendor = async (id: number) => {
+    setLoading(true)
+    try {
+      const response = await api.delete(id.toString())
+      if (!response.ok) throw new Error('Error deleting vendor')
+      const { error, message } = await response.json<{ error: string; message: 'ok' | 'error' }>()
+      if (error) throw new Error(message)
+
+      // remove vendor
+      setVendors((prev) => prev.filter((v) => v.id !== id))
+
+      return true
+    } catch (error) {
+      console.error('Error deleting vendor', error)
+      setError(error as string)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getVendors()
+  }, [])
+
+  return { vendors, loading, error, getVendors, createVendor, updateVendor, deleteVendor }
 }
