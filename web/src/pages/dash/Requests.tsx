01650cb27b0a2da9293a6d4a5233a604ada84915
Dave Njoroge <davenjoroge10@gmail.com>
2025-06-18 23:16:46 +0300

    basic requests page

--- web/src/pages/dash/Requests.tsx
+++ web/src/pages/dash/Requests.tsx
@@ -1,3 +1,4 @@
+import { Badge } from '@/components/ui/badge'
 import DefaultLayout from '../../layouts/Default'
 import { useRequestService } from '../../services/useRequestService'
 
@@ -8,21 +9,31 @@ const Requests = () => {
   return (
     <DefaultLayout>
       <div>
-        <div className="py-3">
-          <h1 className="display-6">Requests</h1>
+        <div className="py-4">
+          <h1 className="text-4xl">Requests</h1>
         </div>
         <div className="mt-4">
-          <ul className="list-group">
+          <ul className="space-y-4">
             {!requests.length && (
-              <li className="list-group-item">
-                <h5 className='mb-0 fw-normal'>No requests atm.</h5>
+              <li className="">
+                <h5 className="text-lg">No requests atm.</h5>
               </li>
             )}
 
             {requests.map((request) => (
-              <li className="list-group-item">
-                <p>{request.id}</p>
-                <p>{request.name}</p>
+              <li className="" key={request.id}>
+                <a href={`#"${request.name}"`} className="border block rounded-lg p-4">
+                  <div className="flex items-center space-x-4">
+                  <p className="font-medium text-lg">{request.name}</p>
+                  <Badge>{request.status}</Badge>
+                  </div>
+                  <p className="text-gray-500">{request.phone}</p>
+                  <ul className="flex items-center gap-2">
+                    {request.originalMessages.map((message) => (
+                      <li>{message}</li>
+                    ))}
+                  </ul>
+                </a>
               </li>
             ))}
           </ul>
