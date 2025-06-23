1a5384aba9c36fdb1420c5d942844ce2fd814887
Dave Njoroge <davenjoroge10@gmail.com>
2025-06-23 23:08:55 +0300

    api: vendor findall query

--- api/src/services/vendor/vendor.service.ts
+++ api/src/services/vendor/vendor.service.ts
@@ -1,10 +1,30 @@
 import { VendorModel } from "../../db/sequelize";
 import type { Vendor, NewVendor } from "../../db/models/vendor.model";
+import { Op } from "sequelize";
 
+type FindVendorParams = Partial<Record<keyof Vendor, any>>;
 class VendorService {
-  static async findAll(where = {}) {
+  static async findAll({ where = {} }: { where?: FindVendorParams } = {}) {
+    const whereParams = {} as Record<keyof Vendor, any>;
+    if (where.brands) {
+      whereParams.brands = {
+        [Op.contains]: [where.brands],
+      };
+      delete where.brands; // Remove from where to avoid conflict
+    }
+
+    if (where.location) {
+      whereParams.location = location;
+    }
+
+    if (where.name) {
+      whereParams.name = {
+        [Op.iLike]: `%${where.name}%`,
+      };
+    }
+
     return await VendorModel.findAll({
-      where,
+      where: whereParams,
       order: [["id", "DESC"]],
     });
   }
