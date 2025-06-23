dd054c5e834a4d7bbd31494c2f4e31c7937b3498
Dave Njoroge <davenjoroge10@gmail.com>
2025-06-23 23:12:14 +0300

    update requests page, new drawer to create/display vendor-requests

--- web/src/pages/dash/Requests.tsx
+++ web/src/pages/dash/Requests.tsx
@@ -1,14 +1,48 @@
 import { Badge } from '@/components/ui/badge'
 import useRequestStore from '@/stores/useRequestStore'
 import DashboardLayout from '@/layouts/Dashboard'
-import { useEffect } from 'react'
 
+import { useEffect, useState } from 'react'
+// import { useClearHash, useHashEffect } from '@/utils/useHashEffect'
+import { Drawer, DrawerContent } from '@/components/ui/drawer'
+import type { Request } from '@/services/useRequestService'
+import RequestDisplay from '@/components/partials/RequestDisplay'
 const Requests = () => {
   const { requests, getRequests } = useRequestStore()
 
   useEffect(() => {
     getRequests()
   }, [])
+
+  const [open, setOpen] = useState<boolean>(false)
+  const [displayRequest, setDisplayRequest] = useState<Request | undefined>()
+
+/*
+  useHashEffect((id) => {
+    if (id) {
+      const request = requests.find((r) => r.id === id)
+      if (!request) return
+      setOpen(true)
+      setDisplayRequest(request)
+    } else {
+      console.log('no id')
+      // if(open) setOpen(false)
+    }
+  })
+
+  const clearHash = useClearHash()
+*/
+  const hideDrawer = () => {
+    setOpen(false)
+    setDisplayRequest(undefined)
+  }
+
+  const showRequest = (id: number) => {
+    const request = requests.find((r) => r.id === id)
+    if (!request) return
+    setOpen(true)
+    setDisplayRequest(request)
+  }
 
   // getRequests
   return (
@@ -27,7 +61,11 @@ const Requests = () => {
 
             {requests.map((request) => (
               <li className="" key={request.id}>
-                <a href={`#"${request.name}"`} className="border block rounded-lg p-4">
+                <a
+                  href={`#${request.id}`}
+                  className="border block rounded-lg p-4"
+                  onClick={() => showRequest(request.id)}
+                >
                   <div className="flex items-center space-x-4">
                     <p className="font-medium text-lg">{request.name}</p>
                     <Badge>{request.status}</Badge>
@@ -44,6 +82,11 @@ const Requests = () => {
           </ul>
         </div>
       </div>
+      <Drawer open={open}>
+        <DrawerContent>
+          <RequestDisplay request={displayRequest} hideDrawer={hideDrawer}></RequestDisplay>
+        </DrawerContent>
+      </Drawer>
     </DashboardLayout>
   )
 }
