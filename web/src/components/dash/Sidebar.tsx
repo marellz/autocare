0ad8547233b4c064260c9b0d19d7e87045a17d80
Dave Njoroge <davenjoroge10@gmail.com>
2025-07-07 18:08:40 +0300

    sidebar update, dark-toggle- wip

--- web/src/components/dash/Sidebar.tsx
+++ web/src/components/dash/Sidebar.tsx
@@ -1,4 +1,15 @@
-import { Home, ListOrdered, Store } from 'lucide-react'
+import {
+  ChevronDown,
+  ChevronUp,
+  Home,
+  LifeBuoy,
+  ListOrdered,
+  LogOut,
+  Send,
+  Store,
+  User2,
+  type LucideIcon,
+} from 'lucide-react'
 import {
   Sidebar,
   SidebarContent,
@@ -12,9 +23,23 @@ import {
   SidebarMenuButton,
 } from '@/components/ui/sidebar'
 import { NavLink } from 'react-router-dom'
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '../ui/collapsible'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
+// import { Button } from '../ui/button'
+
+interface LinkItem {
+  label: string
+  path: string
+  icon: LucideIcon
+}
 
 const DashSidebar = () => {
-  const links = [
+  const links: LinkItem[] = [
     {
       path: '/dashboard/',
       label: 'Dashboard',
@@ -31,30 +56,101 @@ const DashSidebar = () => {
       icon: Store,
     },
   ]
+
+  const helpLinks: LinkItem[] = [
+    {
+      path: '/support',
+      label: 'Support',
+      icon: LifeBuoy,
+    },
+    {
+      path: '/feedback',
+      label: 'Feedback',
+      icon: Send,
+    },
+  ]
+
+  const logout = () => {}
+
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader></SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
-            <SidebarMenu>
-              {links.map((item) => (
-                <SidebarMenuItem key={item.label}>
-                  <SidebarMenuButton asChild>
-                    <NavLink to={item.path}>
-                      <item.icon />
-                      <span>{item.label}</span>
-                    </NavLink>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
+            <SidebarMenuLinks links={links} />
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter>
+        <Collapsible>
+          <SidebarGroup>
+            <SidebarGroupLabel>Settings</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                <SidebarMenuItem>
+                  <SidebarMenuButton asChild>
+                    <span>Dark mode</span>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+          <SidebarGroup>
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                <span className="flex-auto text-left">Help</span>
+                <ChevronDown></ChevronDown>
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenuLinks links={helpLinks} />
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> <span>Username</span>
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent side="top" className="w-[var(--radix-popper-anchor-width)]">
+                <DropdownMenuItem onClick={logout} className="justify-between">
+                  <span>Sign out</span>
+                  <LogOut />
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
+  )
+}
+interface SidebarMenuLinkProps {
+  links: LinkItem[]
+}
+
+const SidebarMenuLinks = ({ links }: SidebarMenuLinkProps) => {
+  return (
+    <SidebarMenu>
+      {links.map((item) => (
+        <SidebarMenuItem key={item.label}>
+          <SidebarMenuButton asChild>
+            <NavLink to={item.path}>
+              <item.icon />
+              <span>{item.label}</span>
+            </NavLink>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      ))}
+    </SidebarMenu>
   )
 }
 
