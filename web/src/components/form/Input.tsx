ab020a08157c2ade7849c17a8021402298d77cd0
Dave Njoroge <davenjoroge10@gmail.com>
2025-07-08 23:00:10 +0300

    update input component

--- web/src/components/form/Input.tsx
+++ web/src/components/form/Input.tsx
@@ -6,17 +6,20 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label?: string
   text?: string
   prefix?: string
+  margin?: string
+  error?: string | null
 }
 
-const Input = ({ type = 'text', label, text, ...props }: Props) => {
+const Input = ({ type = 'text', label, text, margin = 'mb-4', error, ...props }: Props) => {
   const id = useId()
 
   return (
-    <div className="mb-4 space-y-2">
+    <div className={`space-y-2 ${margin}`}>
       {label && <Label htmlFor={id}>{label}</Label>}
       <div>
-        <CustomInput type={type} id={id} {...props} />
+        <CustomInput className={`${error&&'border-red-500'}`} type={type} id={id} {...props} />
       </div>
+      {error && <p className="text-xs text-red-500">{error}</p>}
       {text && <p className="text-sm text-gray-500">{text}</p>}
     </div>
   )
