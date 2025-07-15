603cd00170f56e47f869b9981a736f3a38521143
Dave Njoroge <davenjoroge10@gmail.com>
2025-07-15 19:11:19 +0300

    update client response dialog

--- web/src/components/partials/requests/ClientResponse.tsx
+++ web/src/components/partials/requests/ClientResponse.tsx
@@ -1,4 +1,4 @@
-import type { Request } from '@/services/useRequestService'
+import type { Request, RequestStatus } from '@/services/useRequestService'
 import {
   Dialog,
   DialogContent,
@@ -8,6 +8,24 @@ import {
   DialogTitle,
 } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
+import { AlertCircle, SendHorizontal } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Textarea } from '@/components/ui/textarea'
+import StatusSelect from '@/components/custom/requests/StatusSelect'
+import { useState } from 'react'
+import useResponseStore from '@/stores/useResponseStore'
+import useRequestStore from '@/stores/useRequestStore'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 
 interface Props {
   open: boolean
@@ -16,8 +34,36 @@ interface Props {
 }
 
 const ClientResponse = ({ open, hideDialog, request }: Props) => {
+  const { error, loading, sendClientResponse } = useResponseStore()
+  const { updateRequest } = useRequestStore()
+  const formSchema = z.object({
+    message: z.string().min(1, 'Message is required'),
+  })
+
+  type FormSchema = z.infer<typeof formSchema>
+
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: '',
+    },
+  })
+
+  const [status, setStatus] = useState<RequestStatus>(request?.status || 'submitted')
+
   const handleOpenChange = (show: boolean) => {
     if (!show) hideDialog()
+  }
+
+  const handleSubmit = async ({ message }: FormSchema) => {
+    if (!request) return //todo: throw error
+    // send message
+    const response = await sendClientResponse(request.id, message)
+
+    if (response) hideDialog()
+
+    // if change in status, update that too
+    if (status !== request.status) updateRequest(request.id, { status })
   }
 
   return (
@@ -31,11 +77,49 @@ const ClientResponse = ({ open, hideDialog, request }: Props) => {
               </DialogTitle>
               <DialogDescription>Respond to client concerning their request</DialogDescription>
             </DialogHeader>
+
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(handleSubmit)}>
+                <div className="space-y-4">
+                  <FormField
+                    control={form.control}
+                    name="message"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Send message to client</FormLabel>
+                        <FormControl>
+                          <Textarea {...field}></Textarea>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  ></FormField>
+
+                  <FormItem>
+                    <FormLabel>Change status</FormLabel>
+                    <StatusSelect
+                      status={status}
+                      onSelect={(status: RequestStatus) => setStatus(status)}
+                    />
+                  </FormItem>
+                </div>
+              </form>
+            </Form>
+            {error && (
+              <Alert variant="destructive">
+                <AlertCircle />
+                <AlertTitle>Error sending response</AlertTitle>
+                <AlertDescription>{error}</AlertDescription>
+              </Alert>
+            )}
             <DialogFooter>
               <Button type="button" variant="outline" onClick={hideDialog}>
                 Close
               </Button>
-              <Button>Send response</Button>
+              <Button disabled={loading} onClick={form.handleSubmit(handleSubmit)}>
+                <span>Send response</span>
+                <SendHorizontal />
+              </Button>
             </DialogFooter>
           </>
         ) : (
