be6fc112d76134be2cdfd362f7cc8c5e9be184e5
Dave Njoroge <davenjoroge10@gmail.com>
2025-07-23 21:05:30 +0300

    update recaptcha

--- web/src/components/utils/ReCaptcha.tsx
+++ web/src/components/utils/ReCaptcha.tsx
@@ -2,10 +2,13 @@ import { Turnstile } from '@marsidev/react-turnstile'
 import { Alert, AlertTitle } from '../ui/alert'
 import { AlertCircle } from 'lucide-react'
 import { useTheme } from '../theme/Provider'
+import { useIsMobile } from '@/hooks/use-mobile'
+
 interface Props {
   onSuccess?: (token: string) => void
 }
 const ReCaptcha = ({ onSuccess }: Props) => {
+  const isMobile = useIsMobile()
   const { theme } = useTheme()
   const siteKey = import.meta.env.VITE_CF_TURNSTILE_SITE_KEY
   if (!siteKey)
@@ -21,6 +24,7 @@ const ReCaptcha = ({ onSuccess }: Props) => {
       siteKey={siteKey}
       onSuccess={onSuccess}
       options={{
+        size: isMobile ? 'compact' : 'normal',
         theme: theme === 'system' ? undefined : theme,
       }}
     />
