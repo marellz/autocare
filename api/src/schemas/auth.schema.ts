a45fa61fb30f85b52773a07ea7915ba258d4efbf
Dave Njoroge <davenjoroge10@gmail.com>
2025-07-23 18:29:48 +0300

    api: fix: email -> username in loginFormSchema

--- api/src/schemas/auth.schema.ts
+++ api/src/schemas/auth.schema.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const loginFormSchema = z.object({
-  email: z.string().email(),
+  username: z.string().email(),
   password: z.string().min(6, "Password must be at least 6 characters long"),
 });
 
