80149b8c5d7809412c0e9dff3582df562237f68f
Dave Njoroge <davenjoroge10@gmail.com>
2025-07-29 13:51:44 +0300

    authenticate faq routes

--- api/src/router/routes/_faq.ts
+++ api/src/router/routes/_faq.ts
@@ -3,13 +3,25 @@ import { asyncHandler } from "../../handlers/async.handler";
 import FAQController from "../../controllers/faq.controller";
 import { validate } from "../../handlers/validation.handler";
 import { faqSchema } from "../../schemas/faq.schema";
+import { ensureAuthenticated } from "../../middleware/isAuthenticated";
+
 const router = express.Router();
 
 const { getAll, create, update, destroy } = FAQController;
 
 router.get("/", asyncHandler(getAll));
-router.post("/", validate(faqSchema), asyncHandler(create));
-router.put("/:id", validate(faqSchema.partial()), asyncHandler(update));
-router.delete("/:id", asyncHandler(destroy));
+router.post(
+  "/",
+  ensureAuthenticated,
+  validate(faqSchema),
+  asyncHandler(create),
+);
+router.put(
+  "/:id",
+  ensureAuthenticated,
+  validate(faqSchema.partial()),
+  asyncHandler(update),
+);
+router.delete("/:id", ensureAuthenticated, asyncHandler(destroy));
 
 export default router;
