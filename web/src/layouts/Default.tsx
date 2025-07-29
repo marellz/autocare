0e605bd1bfd9e3e9e72f1fc8f6983dcced6d3a41
Dave Njoroge <davenjoroge10@gmail.com>
2025-07-29 11:53:12 +0300

    update default header/footer layout

--- web/src/layouts/Default.tsx
+++ web/src/layouts/Default.tsx
@@ -17,9 +17,9 @@ import {
   DropdownMenuItem,
 } from '@/components/ui/dropdown-menu'
 import { Button } from '@/components/ui/button'
-import { Blocks, ChevronDown, LogOut, User } from 'lucide-react'
+import { Blocks, ChevronDown, FilePlus2, LogOut, Menu, User } from 'lucide-react'
 import DefaultLayoutToggler from '@/components/theme/DefaultLayoutToggler'
-
+import { useIsMobile } from '@/hooks/use-mobile'
 
 const DefaultLayout = () => {
   const { user, logout } = useAuthStore()
@@ -44,65 +44,89 @@ const DefaultLayout = () => {
 
   const year = new Date().getFullYear()
 
+  const isMobile = useIsMobile()
   const onLogout = () => {
     logout()
   }
   return (
     <>
       <header>
-        <div className="container mx-auto py-8">
-          <div className="flex flex-col items-center justify-between">
+        <div className="container mx-auto py-8 px-2 md:px-4">
+          <div className="flex items-center md:justify-between space-x-4">
             <Link to="/" className="font-bold text-lg">
               Autocare
             </Link>
-            <NavigationMenu className="w-full max-w-full">
-              <NavigationMenuList className="w-full">
-                {headerLinks.map(({ path, label }, i) => (
-                  <NavigationMenuItem key={`${label}-${i}`}>
-                    <NavigationMenuLink asChild>
+            {isMobile ? (
+              <DropdownMenu>
+                <DropdownMenuTrigger className="order-last ml-4 mr-0">
+                  <Menu />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  {headerLinks.map(({ path, label }, i) => (
+                    <DropdownMenuItem key={i} asChild>
                       <Link to={path} className={clsx(location.pathname === path && 'bg-accent')}>
                         {label}
                       </Link>
-                    </NavigationMenuLink>
-                  </NavigationMenuItem>
-                ))}
-                {location.pathname === '/' && (
-                  <NavigationMenuItem>
-                    <NavigationMenuLink href="#faqs">Faqs</NavigationMenuLink>
-                  </NavigationMenuItem>
-                )}
-              </NavigationMenuList>
-            </NavigationMenu>
-            <div className='flex space-x-2'>
-              <DefaultLayoutToggler />
-            {user === null ? (
-              <RequestForm />
-            ) : (
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="ghost">
-                    <User />
-                    <span>{user.name}</span>
-                    <ChevronDown />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuLabel>User menu</DropdownMenuLabel>
-                  <DropdownMenuItem asChild>
-                    <Link to="/dashboard">
-                      <span>Dashboard</span>
-                      <Blocks />
-                    </Link>
-                  </DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem variant="destructive" onClick={onLogout}>
-                    <span>Logout</span>
-                    <LogOut className="ml-auto" />
-                  </DropdownMenuItem>
+                    </DropdownMenuItem>
+                  ))}
                 </DropdownMenuContent>
               </DropdownMenu>
+            ) : (
+              <NavigationMenu className="w-full max-w-full">
+                <NavigationMenuList className="w-full">
+                  {headerLinks.map(({ path, label }, i) => (
+                    <NavigationMenuItem key={`${label}-${i}`}>
+                      <NavigationMenuLink asChild>
+                        <Link to={path} className={clsx(location.pathname === path && 'bg-accent')}>
+                          {label}
+                        </Link>
+                      </NavigationMenuLink>
+                    </NavigationMenuItem>
+                  ))}
+                  {location.pathname === '/' && (
+                    <NavigationMenuItem>
+                      <NavigationMenuLink href="#faqs">Faqs</NavigationMenuLink>
+                    </NavigationMenuItem>
+                  )}
+                </NavigationMenuList>
+              </NavigationMenu>
             )}
-
+            <div className="flex space-x-4 !ml-auto">
+              {!isMobile && <DefaultLayoutToggler />}
+              {user === null ? (
+                <RequestForm
+                  buttonChildren={
+                    <>
+                      <span className="hidden md:inline">Make request</span>
+                      <FilePlus2 />
+                    </>
+                  }
+                />
+              ) : (
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="ghost">
+                      <User />
+                      <span className="hidden md:inline">{user.name}</span>
+                      <ChevronDown />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>User menu</DropdownMenuLabel>
+                    <DropdownMenuItem asChild>
+                      <Link to="/dashboard">
+                        <span>Dashboard</span>
+                        <Blocks />
+                      </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem variant="destructive" onClick={onLogout}>
+                      <span>Logout</span>
+                      <LogOut className="ml-auto" />
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              )}
             </div>
           </div>
         </div>
@@ -116,7 +140,7 @@ const DefaultLayout = () => {
         <div className="container mx-auto py-2 px-4 text-sm text-muted-foreground">
           <div className="flex flex-col-reverse md:flex-row md:justify-between">
             <p className="mt-2 md:mt-0">© {year} Alex Autocare. All rights reserved.</p>
-            <div className="flex">
+            <div className="flex items-center">
               {user === null && (
                 <Link className="hover:underline px-2" to="/login">
                   Login
@@ -127,6 +151,12 @@ const DefaultLayout = () => {
                   {label}
                 </Link>
               ))}
+              {isMobile && (
+                <div className="ml-auto">
+                  {' '}
+                  <DefaultLayoutToggler />
+                </div>
+              )}
             </div>
           </div>
         </div>
