c75aec431ba02904d5a83ed5b30ae1c3ce5d6829
Dave Njoroge <davenjoroge10@gmail.com>
2025-07-30 12:33:54 +0300

    api: controller/service - pagination for contacts

--- api/src/controllers/contact.controller.ts
+++ api/src/controllers/contact.controller.ts
@@ -1,12 +1,54 @@
 import { NextFunction, Request, Response } from "express";
 import service from "../services/contact/contact.service";
 import verifyToken from "../services/recaptcha/verify.service";
+import { FindOptions, Op, WhereOptions } from "sequelize";
+import { ContactMessage } from "src/db/models/contact.model";
 
 class ContactController {
   static async getAll(req: Request, res: Response, next: NextFunction) {
     try {
-      const data = await service.getAll();
-      res.json({ message: "ok", data });
+      const params: FindOptions<ContactMessage> = {};
+      let where: WhereOptions<ContactMessage> = {};
+      const {
+        query,
+        status,
+        page = 1,
+        limit = 10,
+        sort_by = "createdAt",
+        sort_order = "desc",
+      } = req.query;
+      params.limit = parseInt(limit as string, 10);
+      params.offset = (parseInt(page as string, 10) - 1) * params.limit;
+      params.order = [[sort_by as string, sort_order as string]];
+
+      if (query) {
+        where = {
+          [Op.or]: [
+            { name: { [Op.iLike]: `%${query}%` } },
+            { email: { [Op.iLike]: `%${query}%` } },
+            { phone: { [Op.iLike]: `%${query}%` } },
+            { message: { [Op.iLike]: `%${query}%` } },
+          ],
+        };
+      }
+
+      if (status) {
+        where = {
+          ...where,
+          status: { [Op.eq]: status as string },
+        };
+      }
+
+      params.where = where;
+
+      const { items, pagination } = await service.paginate(params);
+      res.json({
+        message: "ok",
+        data: {
+          items,
+          pagination: { ...pagination, page, limit, sort_by, sort_order },
+        },
+      });
     } catch (error) {
       next(error);
     }
@@ -46,9 +88,14 @@ class ContactController {
   static async update(req: Request, res: Response, next: NextFunction) {
     try {
       const { id } = req.params;
-      const { name, phone, email, message } = req.body;
-      const data = await service.update(id, { name, phone, email, message });
-      res.json({ message: "ok", data });
+      const { status } = req.body;
+      if (typeof status === "undefined") {
+        return res
+          .status(400)
+          .json({ message: "Status is required in request body" });
+      }
+      const [count] = await service.update(id, { status });
+      res.json({ message: "ok", updated: count > 0 });
     } catch (error) {
       next(error);
     }
--- api/src/services/contact/contact.service.ts
+++ api/src/services/contact/contact.service.ts
@@ -1,9 +1,25 @@
+import { FindOptions } from "sequelize";
 import {
   ContactMessage,
   NewContactMessage,
 } from "../../db/models/contact.model";
 
 import { ContactMessageModel } from "../../db/sequelize";
+
+const paginate = async (params: FindOptions<ContactMessage>) => {
+  const count = await ContactMessageModel.count({
+    where: params.where,
+  });
+  const items = await ContactMessageModel.findAll(params);
+
+  return {
+    items,
+    pagination: {
+      total: count,
+      page_count: Math.ceil(count / (params.limit || 10)),
+    },
+  };
+};
 
 const getAll = async () => {
   return await ContactMessageModel.findAll({});
@@ -14,16 +30,25 @@ const getById = async (id: string) => {
 };
 
 const create = async (payload: Omit<NewContactMessage, "createdAt">) => {
-  return await ContactMessageModel.create({...payload, createdAt: new Date()});
-};
-
-const update = async (id: string, payload: Partial<ContactMessage>) => {
-  return await ContactMessageModel.update({...payload, updatedAt: new Date()}, {
-    where: { id },
+  return await ContactMessageModel.create({
+    ...payload,
+    createdAt: new Date(),
   });
 };
 
+const update = async (id: string, payload: Partial<ContactMessage>) => {
+  payload.updatedAt = new Date();
+  console.log({payload})
+  return await ContactMessageModel.update(
+    payload,
+    {
+      where: { id },
+    },
+  );
+};
+
 export default {
+  paginate,
   getAll,
   getById,
   create,
