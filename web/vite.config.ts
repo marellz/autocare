0fdc8e129d1f411057ec43354e184a3fb7c9a948
Dave Njoroge <davenjoroge10@gmail.com>
2025-10-23 13:28:42 +0300

    install visualizer for build insight, add bundle-report to gitignore

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      filename: 'bundle-report.html',
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
